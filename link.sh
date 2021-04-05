0e0c05be9c5aa8e1ce51ec8c583d0caba747dd32
riswu <sugike0301@gmail.com>
2021-04-05 21:37:12 +0900

    リンクを張るときにディレクトリを作成

--- link.sh
+++ link.sh
@@ -3,6 +3,9 @@
 ln -snf ~/dotfiles/.zprofile ~/.zprofile
 ln -snf ~/dotfiles/.zshrc ~/.zshrc
 ln -snf ~/dotfiles/.Brewfile ~/.Brewfile
+mkdir -p ~/.config
 ln -snf ~/dotfiles/.config/fish ~/.config/fish
+mkdir -p ~/.config
 ln -snf ~/dotfiles/.config/karabiner ~/.config/karabiner
+mkdir -p ~/Library/Application\ Support/Code
 ln -snf ~/dotfiles/Library/Application\ Support/Code/User ~/Library/Application\ Support/Code/User
